6288ae4efa6dad7021e179094eef52b5c70261e0
Buelie <111875719+Buelie@users.noreply.github.com>
2022-11-28 10:14:16 +0800

    Create GoatV0.3.4[beat].py

---
+++ GoatBeat/GoatV0.3.4[beat].py
@@ -0,0 +1,57 @@
+import sys
+import khl
+import random
+import os
+import datetime
+import keyword
+import functools
+import logging
+from os import system , popen
+class wel:
+    def wel():
+        print("Welcome to the goats")
+class rndom:
+    def dice(sx):
+        a1 = random.randint(1,6)
+        b1 = random.randint(1,6)
+        Bot.run()
+    def brand(pn):
+        bran1 = ['方块','梅花','红心','黑桃']
+        bran2 = ['方块','梅花','红心','黑桃']
+        bran3 = ['方块','梅花','红心','黑桃']
+        bran4 = ['方块','梅花','红心','黑桃']
+        brs1 = random.choice(bran1)
+        brs2 = random.choice(bran2)
+        brs3 = random.choice(bran3)
+        brs4 = random.choice(bran4)
+        Bot.run()
+    def brand(nm):
+        brns1 = ['3','4','5','6','7','8','9','10','J','Q','K','A','2']
+        brns2 = ['3','4','5','6','7','8','9','10','J','Q','K','A','2']
+        brns3 = ['3','4','5','6','7','8','9','10','J','Q','K','A','2']
+        brns4 = ['3','4','5','6','7','8','9','10','J','Q','K','A','2']
+        bns1 = random.choice(brns1)
+        bns2 = random.choice(brns2)
+        bns3 = random.choice(brns3)
+        bns4 = random.choice(brns4)
+        Bot.run()
+    def captcha(dig):
+        capdigs = random.randint(1000,9999)
+    def captcha(let):
+        capletters = ['a','b','c','d','e','f','g','h','i','j','k','l','n','m','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','N','M','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+        caplets = random.choice(capletters)
+    def captcha(sym):
+        capsymbols = [' ','~','`','!','@','#','$','%','^','&','*','(',')','_','-','+','=','{','}','[',']','|',':',';','"','<','>',',','.','?','/']
+        capsyms = random.choice(capsymbols)
+class load:
+    def loadng():
+        ld = 1
+        while ld <= 100:
+            print(ld,"%")
+            ld += 1
+            class pyver:
+class pev:                
+    def pyvr():
+        pvr = system("python --version")
+        per = popen("python --version").read()
+        print(per)
